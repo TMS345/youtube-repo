c4b5f7bc8eb15a9178c4d5af7f95477b997cd911
Gershom Gbadebo <gershomog@gmail.com>
2022-07-01 15:51:29 +0000

    recent commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from googleapiclient.discovery import build
 
+import pandas as pd
+
+import sqlalchemy as db
+
 api_key = 'AIzaSyAqEKiUoSQ8sR81kpuaZLJNlsEwmBHFAH0'
 
 youtube = build("youtube", "v3", developerKey=api_key)
@@ -14,6 +18,14 @@ request = youtube.search().list(
         fields="items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))"
 )
 
-response = request.execute()
+data = request.execute()
 
-print(response, end="")
+data_frame = pd.DataFrame.from_dict (data)
+
+engine = db.create_engine('sqlite:///data_base_name.db')
+
+data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+query_result = engine.execute("SELECT * FROM table;").fetchall ()
+
+print(pd.DataFrame(query_result))
